eec0db9b2dcd9df15500c71314251bd5556dc6b6
NozilaXudoyberdiyeva <nozilaxudoyberdiyeva@gmail.com>
2023-02-03 10:07:41 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let elForm = document.querySelector(".form");
+let elFormInput = document.querySelector(".form-input");
+let elCardBody = document.querySelector(".card-body");
+
+elForm.addEventListener("submit", function(evt){
+    evt.preventDefault();
+
+    let newLine = document.createElement("div");
+    let newCheckBox = document.createElement("input");
+
+    newLine.classList.add("alert", "alert-danger", "d-flex", "justify-content-between");
+    newLine.textContent = elFormInput.value;
+
+    newCheckBox.setAttribute("type", "checkbox");
+    newCheckBox.classList.add("form-check-input");
+    newLine.appendChild(newCheckBox);
+
+    elCardBody.appendChild(newLine);
+    elFormInput.value = "";
+
+    newCheckBox.addEventListener("change", function(evt){
+        if(newCheckBox.checked){
+            newLine.classList.remove("alert-danger");
+            newLine.classList.add("alert-dark", "text-decoration-line-through");
+        }else{
+            newLine.classList.add("alert-danger");
+            newLine.classList.remove("alert-dark", "text-decoration-line-through");
+        }
+    })
+});
